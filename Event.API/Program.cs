742a384fa1af2b6fdaf17de14133ac6f3bb6834a
yousef alazer <y.alazer@grandmstg.com>
2021-08-05 17:21:30 +0200

    inatial

---
+++ Event.API/Program.cs
@@ -0,0 +1,36 @@
+using log4net;
+using log4net.Config;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Event.API
+{
+#pragma warning disable CS1591
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+
+#pragma warning restore CS1591
+}
